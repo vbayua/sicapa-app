f1af7e923db3ad03eb4030613327ca07535c4cfb
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-22 14:03:59 +0700

    Update navbar

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,27 +11,49 @@
 <body>
     <div class="grid">
         <header class="bg-white w-full mx-auto border-b-4">
-            <nav class="max-w-7xl mx-auto flex items-center justify-between p-6 lg:px-8">
-                <div class="flex" x-data>
+            <nav class="w-full flex justify-between p-6 lg:px-8">
+                <div class="flex justify-center items-center" x-data>
                     <a href="" class="text-2xl text-blue-500 font-bold">
                         CAPA
                     </a>
-                    <div x-data>
-                        <button type="button" @click="$dispatch('expand')">
-                            Notify
-                        </button>
-                    </div>
+                </div>
+                <div class="dropdown dropdown-bottom dropdown-end" x-data="{show: false}" @click.away="show = false">
+                    <button type="button" class="btn m-1 btn-ghost" @click="show = ! show">
+                        Welcome, {{auth()->user()->name}}
+                    </button>
+
+                    <ul tabindex="0" x-show="show" class="dropdown-content drop-shadow z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a href="#" @click="document.querySelector('#logout-form').submit()">Log out</a>
+                            <form action="/logout" id="logout-form" method="post" class="hidden">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
 
-        <main class="flex grid-rows-1 bg-blue-300 min-h-lvh">
-            <ul class="menu flex-shrink w-52 p-4 min-h-full bg-base-200 text-base-content">
+        <main class="flex grid-rows-1 lg:h-screen">
+            <ul class="menu flex-shrink w-52 p-4 max-md:hidden min-h-full bg-base-200 text-base-content">
                 <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li class="menu-title">
+                    <span class="font-bold">Dashboard</span>
+                </li>
+                <li>
+                    <a href="/capa" navigate>Data CAPA</a>
+                </li>
+
+                <li>
+                    <a href="#data-capa">New Capa</a>
+                </li>
+
+                <li class="menu-title">Settings</li>
+                <li>
+                    <a href="#data-capa">User Settings</a>
+                </li>
             </ul>
-            <div class="flex-1">
+            <div class="flex-1 overflow-y-auto p-4">
                 {{ $slot }}
             </div>
         </main>
